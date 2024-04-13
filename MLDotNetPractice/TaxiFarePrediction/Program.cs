05475760e93c597dd8f0ba695df1b89268a781aa
LiuShangshin <soup1597@gmail.com>
2024-04-13 10:31:34 +0800

    準備事前作業資料

---
+++ MLDotNetPractice/TaxiFarePrediction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TaxiFarePrediction
+{
+    /// <summary>
+    /// ML.NET測試專案:
+    /// - 主題: 透過回歸分析預測價格
+    /// - 前置作業: Data資料夾必須放置要預測的資料集
+    /// - 框架: 至少為NET 6.0
+    /// - 概念: 提供Feature預測出Label
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
